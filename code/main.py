76ee0fc37d94275a95fd6e3b98244862f599d3f0
guyonghao <546405308@qq.com>
2021-02-06 18:28:50 +0800

    init

--- code/main.py
+++ code/main.py
@@ -6,10 +6,19 @@ libpath = os.path.abspath("./lib")
 sys.path.append(libpath)
 
 
+import log
+import global_obj
+
+def init_log(filename = None):
+    obj = log.CFileLog(filename)
+    global_obj.set_obj("logger", obj)
 
 
 def main(config_file):
-    pass
+    init_log()
+
+    log.Sys("start ok")
+
 
 
 
---
+++ code/spider_beike.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from lxml import etree
+
+import spiker
+import tools
+
+
+@tools.check_use_time(30, tools.global_log)
+def start():
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
